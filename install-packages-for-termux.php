d507c8c24493471a38cab3e71df47842b4a99ebc
thangvoquyet46 <78263487+thangvoquyet46@users.noreply.github.com>
2021-10-23 20:59:27 +0700

    Update install-packages-for-termux.php

--- install-packages-for-termux.php
+++ install-packages-for-termux.php
@@ -1,20 +1,35 @@
 <?php
 //before installing, enter the command"pkg install php"
+@system("clear");
 @system("pkg update");
 @system("pkg upgrade");
+@system("clear");
 @system("termux-setup-storage");
+@system("clear");
 @system("pkp i php");
+@system("clear");
 @system("pkg i vim");
+@system("clear");
 @system("pkg i readline");
+@system("clear");
 @system("pkg i python");
+@system("clear");
 @system("pkg i python2");
+@system("clear");
 @system("pkg i git");
+@system("clear");
 @system("pkg i wget");
+@system("clear");
 @system("pkg i vim");
+@system("clear");
 @system("pkg i nano");
+@system("clear");
 @system("pkg i unzip");
+@system("clear");
 @system("pkg i bash");
+@system("clear");
 @system("pkg install root-repo");
+@system("clear");
 @system("pip install --upgrade pip && pip install telethon && pip install requests && pip install bs4 && pip install rsa && pip install pyaes && pip install async_generator && pip install colorama && pip install --upgrade pip");
 @system("clear");
 ?>
