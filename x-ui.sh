6f7cb3bb28d0c6d478c71904be98d07934357819
thangvoquyet46 <78263487+thangvoquyet46@users.noreply.github.com>
2022-03-25 17:22:18 +0700

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -5,12 +5,10 @@ clear
 echo -e "Bắt đầu cài đặt"
 wget https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh
 bash install.sh
-clear
 apt install speedtest-cli -y
 clear
 echo -e "Bắt đầu kiểm tra tốc độ mạng"
 speedtest
-clear
 apt install neofetch -y
 clear
 echo -e "Kiểm tra hệ thống"
