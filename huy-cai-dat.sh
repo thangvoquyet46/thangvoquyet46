982ea8745eb1872d643e55724f31aa81db3c02be
thangvoquyet46 <78263487+thangvoquyet46@users.noreply.github.com>
2022-04-09 15:33:54 +0700

    Create huy-cai-dat.sh

---
+++ huy-cai-dat.sh
@@ -0,0 +1,10 @@
+cd /root/
+rm x-ui/ /usr/local/x-ui/ /usr/bin/x-ui -rf
+tar zxvf x-ui-linux-amd64.tar.gz
+chmod +x x-ui/x-ui x-ui/bin/xray-linux-* x-ui/x-ui.sh
+cp x-ui/x-ui.sh /usr/bin/x-ui
+cp -f x-ui/x-ui.service /etc/systemd/system/
+mv x-ui/ /usr/local/
+systemctl daemon-reload
+systemctl enable x-ui
+systemctl restart x-ui
